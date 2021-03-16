8b9d77e1bb7d6303c661bbd829263c5086c23d0d
Calle <carl@lindstedt.net>
2021-03-16 13:22:13 +0100

    Klar med dagens labb!

--- APILabb/Controllers/PokemonController.cs
+++ APILabb/Controllers/PokemonController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APILabb.Controllers
@@ -7,10 +8,24 @@ namespace APILabb.Controllers
     [Route("api/pokemon")]
     public class PokemonController : ControllerBase
     {
+
+        List<Pokemon> pokemons = new List<Pokemon>(){
+            new Pokemon() {Name = "Charizard", Level = 1},
+            new Pokemon() {Name = "Ditto", Level = 1}
+        };
+
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok("Björnen sover");
+            return Ok(pokemons[0]);
+        }
+
+        [HttpPut]
+        public ActionResult Put(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+
+            return Ok(pokemons);
         }
     }
 }
---
+++ APILabb/Pokemon.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace APILabb
+{
+    public class Pokemon
+    {
+        public string Name { get; set;}
+        public int Level { get; set;}
+    }
+}
